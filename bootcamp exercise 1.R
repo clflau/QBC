61a671a2a5181e0264e4d2ef38458e0c7c0cc859
clflau <clivelflau@gmail.com>
2015-09-16 17:49:12 -0700

    added ans to number 1

--- bootcamp exercise 1.R
+++ bootcamp exercise 1.R
@@ -1,3 +1,10 @@
 #Exercise 
 #1) Write a for loop statements so that it runs from 1:9 and prints the following output to your screen
 
+for(ii in 1:9){
+  if(ii < 9){
+    cat("\n")
+  }
+  else cat("*")
+}
+
