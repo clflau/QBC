10bf426711b27b59e61071ceb319fd38fc6e1447
clflau <clivelflau@gmail.com>
2015-09-21 01:01:36 -0700

    added logistic ode exercise

--- ODEmodel.R
+++ ODEmodel.R
@@ -23,8 +23,65 @@ expGrowthOutput <- lsoda( init, tseq, expGrowthODE, pars)
 plot(expGrowthOutput[,1], expGrowthOutput[,2], col="blue", type="l")
 
 
+#Mini-exercise 4.2.1
+
+logGrowthODE <- function(tt, yy, pars) {
+  derivs <- pars["rr"] * yy*(1-(yy/pars["kk"]))
+  return(list(derivs))
+}
+
+init <- 1
+tseq <- seq(0, 20, by=0.01)
+pars <- c(rr = 2, kk = 20)
+
+logGrowthOutput <- lsoda(init, tseq, logGrowthODE, pars)
+
+plot(logGrowthOutput[,1], logGrowthOutput[,2], col="red", type="l")
 
 
+#Mini-exercise 4.2.2
 
+logGrowthHarvODE <- function(tt, yy, pars) {
+  derivs <- pars["rr"] * yy*(1-(yy/pars["kk"])-pars["hh"]*yy)
+  return(list(derivs))
+}
+
+init <- 1
+tseq <- seq(0, 20, by=0.01)
+pars <- c(rr = 2, kk = 100, hh = 0.9)
+
+logGrowthHarvOutput <- lsoda(init, tseq, logGrowthHarvODE, pars)
+
+plot(logGrowthHarvOutput[,1], logGrowthHarvOutput[,2], col="green", type="l")
+
+###############################################
+#Sensitivity analysis
+
+
+init <- 1
+tseq <- seq(0, 20, by=0.01)
+pars <- c(rr = 0.2, kk = 100, hh = 0.9)
+
+ICVec <- seq(0, 200, by=10)
+plot(x=NULL, y=NULL, xlim=c(1,max(tseq)), ylim=c(0,2*pars["kk"]), xlab="time",
+     ylab="N", main="Logistic growth")
+for (ii in 1:length(ICVec)) {
+  init <- ICVec[ii]
+  logisticOutput <- lsoda(init, tseq, logGrowthODE, pars)
+  lines(logisticOutput[,1], logisticOutput[,2], col="red", type="l")
+}
+
+index <- which(logisticOutput[,1]==5)
+nnVec <- rep(NA, 1, length(ICVec))
+
+for (ii in 1:length(ICVec)) {
+  init <- ICVec[ii]
+  logisticOutput <- lsoda(init, tseq, logGrowthODE, pars)
+  nnVec[ii] <- logisticOutput[index,2]
+}
+
+x11() # if needed
+plot(ICVec, nnVec, xlab="Initial population size",
+     ylab="Population at time=5", type="b", col="red")
 
 
